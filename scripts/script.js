cbaf6e40f4b4c35d7144b609c688188b400c3a33
tupala <mark.tupala@resolver.com>
2021-01-18 18:28:56 -0700

    Revert "Example changes to 2 files"
    This reverts commit caea58f373ef5f13902c1199a54b3d4df268fd2c.

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,6 @@ window.onload = function () {
   var button = document.querySelector("button");
 
   button.addEventListener("click", function () {
-    alert("We're going to Rock!!");
+    alert("Rock!!");
   });
 };

443b36caf6f4263ab8f1d1cedeaa195c1a722087
tupala <mark.tupala@resolver.com>
2021-02-18 20:47:39 -0700

    more to-do list functionality

--- applications/todo-list/app.js
+++ applications/todo-list/app.js
@@ -1,24 +1,41 @@
 const addButton = document.querySelector("#add-button");
+const form = document.querySelector("form");
 const textInput = document.querySelector("#input-description");
+const errorOutput = document.querySelector("#error-output");
 const activeList = document.querySelector("#active-list");
 const completedList = document.querySelector("#completed-list");
 
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+});
+
 addButton.addEventListener("click", () => {
   const description = textInput.value.trim();
-  console.log(`Clicked Add To-Do button with a description of ${description}.`);
+  if (!description) {
+    errorOutput.innerText = "Please provide a description of the to-do item.";
+    return;
+  }
 
   const newItem = document.createElement("li");
   newItem.innerText = description;
 
+  textInput.value = "";
+  errorOutput.innerText = "";
+
   const completeButton = document.createElement("button");
   completeButton.innerText = "Complete";
-  completeButton.id = 1;
   completeButton.addEventListener("click", function () {
-    console.log(this.id);
     completedList.appendChild(this.parentElement);
   });
 
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "Delete";
+  deleteButton.addEventListener("click", function () {
+    this.parentElement.remove();
+  });
+
   newItem.appendChild(completeButton);
+  newItem.appendChild(deleteButton);
 
   activeList.appendChild(newItem);
 });

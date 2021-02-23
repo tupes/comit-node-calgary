9b552e1bcf715d808fee9205d9c258b628cd51d9
tupala <mark.tupala@resolver.com>
2021-02-23 16:28:44 -0700

    Added handlebars

--- applications/sports-ecommerce/server.js
+++ applications/sports-ecommerce/server.js
@@ -1,14 +1,27 @@
 const express = require("express");
+const expressHandlebars = require("express-handlebars");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.engine(
+  "handlebars",
+  expressHandlebars({
+    defaultLayout: "main",
+  })
+);
+app.set("view engine", "handlebars");
+
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-  res.type("text/plain");
-  res.send("Home");
+  res.render("home");
 });
-app.get("/about", (req, res) => {
-  res.type("text/plain");
-  res.send("This is the about page");
+app.get("/signup", (req, res) => {
+  res.render("signup");
+});
+app.post("/signup", (req, res) => {
+  res.send(`Thanks, ${req.body.username}, you have successfully signed-up!`);
 });
 
 app.listen(3000, () => {

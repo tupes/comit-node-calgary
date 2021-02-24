42c17ea7207065187b0c631d18577903f25431d5
tupala <mark.tupala@resolver.com>
2021-02-23 21:21:58 -0700

    More express and handlebars stuff

--- applications/sports-ecommerce/server.js
+++ applications/sports-ecommerce/server.js
@@ -1,16 +1,36 @@
 const express = require("express");
+const expressHandlebars = require("express-handlebars");
+const bodyParser = require("body-parser");
+
+const data = require("./data.json");
 
 const app = express();
 
+// configure express to use handlebars
+app.engine(
+  "handlebars",
+  expressHandlebars({
+    defaultLayout: "main",
+  })
+);
+app.set("view engine", "handlebars");
+
+// middleware
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// routing
 app.get("/", (req, res) => {
-  res.type("text/plain");
-  res.send("Home");
+  res.render("home", { items: data.items, username: "<i>Mark</i>" });
 });
-app.get("/about", (req, res) => {
-  res.type("text/plain");
-  res.send("This is the about page");
+app.get("/signup", (req, res) => {
+  res.render("signup");
+});
+app.post("/submit", (req, res) => {
+  res.send(`Thanks, ${req.body.username}, for signing up!`);
 });
 
+// start the server
 app.listen(3000, () => {
   console.log("Express started on port 3000");
 });

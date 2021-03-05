a8d541c20077d82a3b7d7291ae3bd9afad2d97fb
tupala <mark.tupala@resolver.com>
2021-03-04 18:02:32 -0700

    Changed product schema

--- applications/sports-ecommerce/models/product.js
+++ applications/sports-ecommerce/models/product.js
@@ -1,13 +1,25 @@
 const cuid = require("cuid");
 const db = require("../db.js");
 
+const itemCategories = [
+  "watersports",
+  "soccer",
+  "basketball",
+  "hockey",
+  "boardgames",
+];
+
 const productSchema = db.Schema({
   _id: { type: String, default: cuid },
   name: String,
   price: Number,
   description: String,
   image: String,
-  category: String,
+  category: {
+    type: String,
+    index: true,
+    enum: itemCategories,
+  },
 });
 
 const Product = db.model("Product", productSchema);

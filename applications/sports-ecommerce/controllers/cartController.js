0e8f817c42e4dbe3cff61190b44841dd0c3af694
tupala <mark.tupala@resolver.com>
2021-03-22 16:07:14 -0600

    Added some error handling

--- applications/sports-ecommerce/controllers/cartController.js
+++ applications/sports-ecommerce/controllers/cartController.js
@@ -1,21 +1,29 @@
 const { getUserWithCart } = require("../services/userService.js");
 const cartService = require("../services/cartService.js");
 
-async function renderUserCart(req, res) {
-  const user = await getUserWithCart(req.username);
-  console.log(user.cart);
-  res.render("cart", { products: user.cart });
+async function renderUserCart(req, res, next) {
+  try {
+    const user = await getUserWithCart(req.username);
+    console.log(user.cart);
+    res.render("cart", { products: user.cart });
+  } catch (error) {
+    next(error);
+  }
 }
 
-async function addItemToCart(req, res) {
-  const updatedUser = await cartService.addItemToCart(
-    req.username,
-    req.body.itemId
-  );
-  const cart = updatedUser.cart.map((item) => {
-    return { name: item.name, price: item.price };
-  });
-  res.json({ cart: cart });
+async function addItemToCart(req, res, next) {
+  try {
+    const updatedUser = await cartService.addItemToCart(
+      req.username,
+      req.body.itemId
+    );
+    const cart = updatedUser.cart.map((item) => {
+      return { name: item.name, price: item.price };
+    });
+    res.json({ cart: cart });
+  } catch (error) {
+    next(error);
+  }
 }
 
 module.exports = {
